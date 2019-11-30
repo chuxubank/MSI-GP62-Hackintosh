20207fed6e37c82f04b95240ad76366052408739
Asuna <chuxubank@qq.com>
2019-11-30 11:10:12 +0800

    add HDMI/DP support closed #1

--- sync.sh
+++ sync.sh
@@ -3,4 +3,11 @@ basepath=$(cd `dirname $0`; pwd)
 sudo diskutil mount EFI
 rm -r $basepath/CLOVER
 cp -r /Volumes/EFI/EFI/CLOVER $basepath
+cd $basepath/CLOVER
+cp config.plist config-my.plist
+plutil -remove Devices.Properties.PciRoot\(0\)/Pci\(0x02,0\) config.plist
+plutil -remove SMBIOS config.plist
+plutil -insert SMBIOS -xml "<dict/>" config.plist
+plutil -insert SMBIOS.ProductName -string "MacBookPro13,1" config.plist
+plutil -lint config.plist
 echo "Sync finish."
