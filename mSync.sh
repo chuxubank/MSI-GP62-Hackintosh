3a259ecbc42a956f25388a8128eeb8042848b675
Asuna <chuxubank@qq.com>
2019-12-25 19:21:38 +0800

    uc; use agdpmod=vit9696 to get dp output

--- mSync.sh
+++ mSync.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 basepath=$(cd `dirname $0`; pwd)
-sudo diskutil mount EFI
+EFI=$(mount | grep "/Volumes/EFI")
+if [[ "$EFI" == "" ]]
+then
+    sudo diskutil mount EFI
+fi
 rm -r $basepath/CLOVER
 cp -r /Volumes/EFI/EFI/CLOVER $basepath
 cd $basepath/CLOVER
 cp config.plist config-my.plist
-plutil -replace Boot.Arguments -string "" config.plist
+plutil -replace Boot.Arguments -string "agdpmod=vit9696" config.plist
 plutil -remove SMBIOS config.plist
 plutil -insert SMBIOS -xml "<dict/>" config.plist
-plutil -insert SMBIOS.ProductName -string "MacBookPro13,1" config.plist
+plutil -insert SMBIOS.ProductName -string "MacBookPro13,3" config.plist
 plutil -lint config.plist
 echo "Sync finish."
